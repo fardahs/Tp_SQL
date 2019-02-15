14e33377f0a6b70a163c305fe11f91b4afe451e2
Farda HOUMADI <farda.houmadi@hotmail.fr>
2019-02-15 15:17:10 +0100

    Fonction

---
+++ Exo5.sql
@@ -0,0 +1,11 @@
+-- Affiche la somme des salaires et des commissions des employés.
+SELECT SUM(SALAIRE) AS Salaires, SUM(COMMISSION) AS Commission FROM employes;
+
+-- Affiche la moyenne des salaires et des commissions des employés.
+SELECT AVG(SALAIRE) AS Salaires, AVG(COMMISSION) AS Commission FROM employes;
+
+-- Affiche le salaire maximum et la plus petite commission des employés.
+SELECT MAX(SALAIRE) AS Salaires, MIN(COMMISSION) AS Commission FROM employes;
+
+-- Affiche le nombre distinct de fonction.
+SELECT DISTINCT COUNT(FONCTION) AS Fonction FROM employes;
