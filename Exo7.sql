1c73ae1c3a1f59c381899ebd7a7d4011bee3036d
Farda HOUMADI <farda.houmadi@hotmail.fr>
2019-02-18 10:33:48 +0100

    Listes produits not exists

--- Exo7.sql
+++ Exo7.sql
@@ -1,4 +1,4 @@
--- Affiche la société, adresse et ville de résidence pour tous les tiers de l’entreprise.
+-- Affiche la société, adresse et ville de résidence pour tous les tiers de l’entreprise(client et fournisseur).
 SELECT SOCIETE, ADRESSE, VILLE FROM clients
 UNION
 SELECT SOCIETE, ADRESSE, VILLE FROM fournisseurs;
@@ -19,16 +19,13 @@ WHERE (CODE_CATEGORIE  = 2 AND NO_FOURNISSEUR = 2)
 AND dc.NO_COMMANDE = dc2.NO_COMMANDE);
 
 -- Affiche la liste des produits que les clients parisiens ne commandent pas.
-SELECT dc.REF_PRODUIT AS Produit FROM details_commandes dc
-JOIN commandes c
-ON dc.NO_COMMANDE = c.NO_COMMANDE
-JOIN clients cl
-ON c.CODE_CLIENT = cl.CODE_CLIENT
-WHERE cl.VILLE = "Paris"
-AND NOT EXISTS(
-SELECT dc2.REF_PRODUIT AS Produit FROM details_commandes dc2
+SELECT * FROM produits
+WHERE NOT EXISTS(
+SELECT dc2.REF_PRODUIT AS Produit 
+FROM details_commandes dc2
 JOIN commandes c2
-ON dc2.NO_COMMANDE = c2.NO_COMMANDE
+ON c2.NO_COMMANDE = dc2.NO_COMMANDE
 JOIN clients cl2
 ON c2.CODE_CLIENT = cl2.CODE_CLIENT
-WHERE cl2.VILLE = "Paris" AND c2.NO_COMMANDE IS NULL );
+WHERE cl2.VILLE = "Paris" AND dc2.REF_PRODUIT = produits.REF_PRODUIT
+)
